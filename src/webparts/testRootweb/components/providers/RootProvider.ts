0e4551708f15a5a32440ed56e146dabbedcc37d2
Walter Cruzate <wcruzate@loeb.com>
2022-04-16 20:19:17 -0400

    added comments at the point it fails

--- src/webparts/testRootweb/components/providers/RootProvider.ts
+++ src/webparts/testRootweb/components/providers/RootProvider.ts
@@ -12,6 +12,13 @@ export class RootProvider {
 
     public getRootTitle = async ():Promise<string> => {
 
+        /*/ This call is ok when opening the page directly
+        // but after going to another page, and then hitting
+        // the back button on the browser, it loses
+        // pageContext. The error says 
+        // uncaught (in promise) TypeError: Cannot read properties
+        // of undefined (reading 'web')
+        */
         const rootweb = await this._sp.site.getRootWeb();
 
         const { Title } = await rootweb.select("Title")();
